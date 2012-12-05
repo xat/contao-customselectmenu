c6a8c523eff82fba07b3700a59046ea0593d8eec
Christoph Wiechert <wio@psitrax.de>
2012-12-05 23:00:05 +0100

    Added: Hack to support Contao version 2.11.x

---
+++ system/modules/customselectmenu/CustomSelectMenu.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Contao 2.x VERSION HACK
+ */
+
+require_once(dirname(__FILE__).'/widgets/CustomSelectMenu.php');
+class CustomSelectMenu extends \Sope\CustomSelectMenu\CustomSelectMenu {}
